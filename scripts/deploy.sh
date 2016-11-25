60619abfc4a261c4868419a87ccb62c01e543b50
Brad Pitcher <bradpitcher@gmail.com>
2016-11-25 09:47:57 -0800

    reduce process check timeout to 1 minute

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,9 +2,9 @@
 
 npm run deploy &
 
-# Output to the screen every 9 minutes to prevent a travis timeout
+# Output to the screen every minute to prevent a travis timeout
 export PID=$!
 while [[ `ps -p $PID | tail -n +2` ]]; do
   echo 'Deploying'
-  sleep 540
+  sleep 60
 done
