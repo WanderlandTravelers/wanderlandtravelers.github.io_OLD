fa00d791ae735dea6e0cc59d3bea730bfd14a48e
Brad Pitcher <bradpitcher@gmail.com>
2016-12-09 10:12:53 -0800

    fix font awesome in production build

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,12 +5,17 @@ exports.modifyWebpackConfig = function (config, env) {
 
   config.removeLoader('images')
   config.loader('images', {
-    test: /\.(jpe?g|png|gif|svg)(\?.*)?$/i,
+    test: /\.(jpe?g|png|gif)(\?.*)?$/i,
     loaders: env == 'develop' ? ['url-loader?limit=10000'] : [
       'url-loader?limit=10000',
       'responsive-loader',
       'image-webpack-loader?{progressive:true, optimizationLevel: 7, interlaced: false, pngquant:{quality: "99-100", speed: 4}}',  // eslint-disable-
     ],
+  })
+  config.removeLoader('file-loader')
+  config.loader('file-loader', {
+    test: /\.(ico|eot|otf|webp|ttf|svg)(\?.*)?$/,
+    loader: 'file',
   })
   return config
 }
